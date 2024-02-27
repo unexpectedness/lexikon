9e18e22ed787868dcd2338884577dc8e2662e48a
unexpectedness <simplertodescribethantogenerate@proton.me>
2024-02-27 01:54:20 +0100

    Bump shuriken to 0.14.52

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lexikon "0.2.1"
+(defproject lexikon "0.2.2"
   :description "Reify, manipulate and replay the lexical environment in Clojure"
   :url "https://github.com/unexpectedness/lexikon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [net.clojars.unexpectedness/shuriken "0.14.1"]]
+                 [net.clojars.unexpectedness/shuriken "0.14.52"]]
   :profiles {:dev {:dependencies [[codox-theme-rdash "0.1.2"]]}}
   :plugins [[lein-codox "0.10.3"]]
   :codox {:source-uri "https://github.com/unexpectedness/threading/" \
